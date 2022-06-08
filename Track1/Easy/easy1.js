73f3e3b1c5a1a3c5643c99a8291c88d2a5e5294b
Ayush Jain <ayushganna67@gmail.com>
2022-06-08 12:27:57 +0530

    [UPDATED] Added CSS to Track 1(Easy)

--- Track1/Easy/easy1.js
+++ Track1/Easy/easy1.js
@@ -1,11 +1,27 @@
-function mySubmitFunction(e) {
-    e.preventDefault();
-    a= document.getElementById("Number").value;
-    a=parseInt(a);
-    if(a < 582)
-        console.log("higher");
-    else if(a > 582)
-        console.log("lOWER");
-    else
+var counter = 0;
+var db;
+var name;
+
+var input = document.getElementById("numInput");
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("guessButton").click();
+    }
+});
+
+function guessNum() {
+    counter++;
+    document.getElementById("counter").innerHTML = counter;
+    var numIn = 0;
+    numIn = document.getElementById("numInput").value;
+    if (numIn > 786) {
+        console.log("Lower");
+    } else if (numIn < 786) {
+        console.log("Higher");
+    } else if (numIn == 786) {
+        document.getElementById("theGuessing").style.display = "none";
         console.log("shorturl.at/kCDP3");
+    }
+    document.getElementById("numInput").value = "";
 }
