0e48d0e9dbb6a6d49461c7feff9606b56d4cf985
divyamseek <divyam.gupta@seekaxis.com>
2022-06-15 12:45:03 +0530

    [UPDATED] Final round Hard question and login page

--- Track1/Hard/hard1.js
+++ Track1/Hard/hard1.js
@@ -22,7 +22,7 @@ function submitClue2(e) {
     document.getElementById("clue").innerHTML =
       `Your answer was correct!!
       Clue 3: Go to our official instagram page and search fo the next clue.
-      Your famous F.R.I.E.N.D.S. character left me at altar. Find me and help me.`;
+      Your famous F.R.I.E.N.D.S. character forgot my name at the altar. Find me and help me.`;
     document.getElementById("clue2").setAttribute("hidden", "true");
     document.getElementById("response").innerText = "";
   } else document.getElementById("response").innerText = "Try Again";
