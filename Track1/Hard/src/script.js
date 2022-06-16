d4ae789d9c1d8d1aed40592d507d986ee8e8f72d
Dhruv Dange <dhruvdange@gmail.com>
2022-06-16 11:36:56 +0530

    [OBFUSCATED]

--- Track1/Hard/src/script.js
+++ Track1/Hard/src/script.js
@@ -1,44 +1,103 @@
+const _0x38e900 = _0x1391;
+(function (_0xb95c72, _0x1bd1f6) {
+  const _0x1bcb20 = _0x1391,
+    _0x4a7bb0 = _0xb95c72();
+  while (!![]) {
+    try {
+      const _0x1c2b2c =
+        (parseInt(_0x1bcb20(0x18f)) / 0x1) *
+          (parseInt(_0x1bcb20(0x18c)) / 0x2) +
+        -parseInt(_0x1bcb20(0x17e)) / 0x3 +
+        (parseInt(_0x1bcb20(0x188)) / 0x4) *
+          (-parseInt(_0x1bcb20(0x187)) / 0x5) +
+        (-parseInt(_0x1bcb20(0x181)) / 0x6) *
+          (-parseInt(_0x1bcb20(0x178)) / 0x7) +
+        -parseInt(_0x1bcb20(0x17a)) / 0x8 +
+        -parseInt(_0x1bcb20(0x179)) / 0x9 +
+        (-parseInt(_0x1bcb20(0x185)) / 0xa) *
+          (-parseInt(_0x1bcb20(0x186)) / 0xb);
+      if (_0x1c2b2c === _0x1bd1f6) break;
+      else _0x4a7bb0["push"](_0x4a7bb0["shift"]());
+    } catch (_0x3427cf) {
+      _0x4a7bb0["push"](_0x4a7bb0["shift"]());
+    }
+  }
+})(_0x1abb, 0xeb79b);
 import "./style.css";
-
-window.onbeforeunload = function () {
-  return "All progress will be lost if you leave!";
-};
-
-window.onload = function () {
-  document.onkeydown = function (e) {
-    return (e.which || e.keyCode) != 116;
+function _0x1391(_0x26ef79, _0x1a5161) {
+  const _0x1abb48 = _0x1abb();
+  return (
+    (_0x1391 = function (_0x1391ff, _0xd95aad) {
+      _0x1391ff = _0x1391ff - 0x176;
+      let _0x4513be = _0x1abb48[_0x1391ff];
+      return _0x4513be;
+    }),
+    _0x1391(_0x26ef79, _0x1a5161)
+  );
+}
+function _0x1abb() {
+  const _0xbc1bd7 = [
+    "8aWzfPd",
+    "onkeydown",
+    "Maybe\x20look\x20inside\x20the\x20page\x20source\x20for\x20a\x20clue\x20&#128064",
+    "201664uAPBoa",
+    "value",
+    ".clue",
+    "All\x20progress\x20will\x20be\x20lost\x20if\x20you\x20leave!",
+    "click",
+    "12412057FtSFwu",
+    "4014702QqsKpy",
+    "9805032eNhNux",
+    "querySelector",
+    "#clue1-submit",
+    "which",
+    "4778856MKiquC",
+    "INSTAGRAM",
+    "onload",
+    "6YEAXnR",
+    "#ans1",
+    "innerHTML",
+    "Try\x20again...",
+    "34143060Lhpbuc",
+    "11YmixER",
+    "565jdNVwV",
+    "62476sehtDy",
+    "preventDefault",
+    "keyCode",
+    "onbeforeunload",
+  ];
+  _0x1abb = function () {
+    return _0xbc1bd7;
   };
-};
-
-const clue2 = `
-<center>
-    <h2 class="clue1-title">
-        Your answer was correct!!
-        Clue 2: Go to our official instagram page and search fo the next clue.
-        Your famous F.R.I.E.N.D.S. character forgot my name at the altar. 
-        Find me and help me.
-          <br />
-         Fine the account and get your next clue!!
-    </h1>
-</center>
-`;
-
-const clue1_submit = document.querySelector("#clue1-submit");
-let count = 0;
-
-clue1_submit.addEventListener("click", (event) => {
-  event.preventDefault();
-  const ans = document.querySelector("#ans1").value;
-  const hint1 = document.querySelector("#hint1");
-  if (ans.toUpperCase() === "INSTAGRAM") {
-    document.querySelector(".clue").innerHTML = clue2;
-  } else {
-    count++;
-    hint1.innerHTML = `Try again...`;
-  }
-
-  if (count >= 8) {
-    hint1.innerHTML = `Maybe look inside the page source for a clue &#128064`;
-  }
-  document.querySelector("#ans1").value = "";
+  return _0x1abb();
+}
+(window[_0x38e900(0x18b)] = function () {
+  const _0x396050 = _0x38e900;
+  return _0x396050(0x176);
+}),
+  (window[_0x38e900(0x180)] = function () {
+    const _0x467dd2 = _0x38e900;
+    document[_0x467dd2(0x18d)] = function (_0x1f6e03) {
+      const _0x190ae6 = _0x467dd2;
+      return (
+        (_0x1f6e03[_0x190ae6(0x17d)] || _0x1f6e03[_0x190ae6(0x18a)]) != 0x74
+      );
+    };
+  });
+const clue2 =
+    "\x0a<center>\x0a\x20\x20\x20\x20<h2\x20class=\x22clue1-title\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20Your\x20answer\x20was\x20correct!!\x0a\x20\x20\x20\x20\x20\x20\x20\x20Clue\x202:\x20Go\x20to\x20our\x20official\x20instagram\x20page\x20and\x20search\x20fo\x20the\x20next\x20clue.\x0a\x20\x20\x20\x20\x20\x20\x20\x20Your\x20famous\x20F.R.I.E.N.D.S.\x20character\x20forgot\x20my\x20name\x20at\x20the\x20altar.\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20Find\x20me\x20and\x20help\x20me.\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<br\x20/>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20Fine\x20the\x20account\x20and\x20get\x20your\x20next\x20clue!!\x0a\x20\x20\x20\x20</h1>\x0a</center>\x0a",
+  clue1_submit = document["querySelector"](_0x38e900(0x17c));
+let count = 0x0;
+clue1_submit["addEventListener"](_0x38e900(0x177), (_0x87777a) => {
+  const _0x372c18 = _0x38e900;
+  _0x87777a[_0x372c18(0x189)]();
+  const _0x242621 = document[_0x372c18(0x17b)](_0x372c18(0x182))[
+      _0x372c18(0x190)
+    ],
+    _0x2e448f = document[_0x372c18(0x17b)]("#hint1");
+  _0x242621["toUpperCase"]() === _0x372c18(0x17f)
+    ? (document["querySelector"](_0x372c18(0x191))[_0x372c18(0x183)] = clue2)
+    : (count++, (_0x2e448f[_0x372c18(0x183)] = _0x372c18(0x184))),
+    count >= 0x8 && (_0x2e448f[_0x372c18(0x183)] = _0x372c18(0x18e)),
+    (document[_0x372c18(0x17b)](_0x372c18(0x182))[_0x372c18(0x190)] = "");
 });
--- Track3/hard/src/script.js
+++ Track3/hard/src/script.js
@@ -1,55 +1,138 @@
-import "./style.css";
-
-window.onbeforeunload = function () {
-  return "All progress will be lost if you leave!";
-};
-
-window.onload = function () {
-  document.onkeydown = function (e) {
-    return (e.which || e.keyCode) != 116;
-  };
-};
-
-const clue2 = `
-<div id = "clue2">
-        <img src = "../static/1.jpg">
-        <p>Sanjay Singhania recently joined Stack Overflow. Your next clue is hidden in his Stack Overflow profile. But because of his short term memory loss problem, he has forgotten his Stack Overflow Display Name. All he remembers is this meme template below -</p>
-        <img src = "../static/2.jpg">
-        <p>Can YOU help him remember his username???</p>
-    <form id="clue2-form">
-      <input type="text" id="ans2" />
-      <input type="submit" value="Check" id="input2" />
-    </form>
-  
-</div>
-`;
-
-const submitClue1 = (e) => {
-  e.preventDefault();
-  let a = document.getElementById("ans1").value;
-  console.log(a);
-  if (a.toLowerCase() == "stack overflow" || a.toLowerCase == "stackoverflow") {
-    document.getElementById("response").innerText = "";
-    document.getElementById("clue").innerHTML =
-      "Your answer was correct!!<br><br>Here's your next clue.";
-    //document.getElementById("hint").innerText = "";
-    document.getElementById("hint").innerHTML = clue2;
-    document.getElementById("input2").addEventListener("click", submitClue2);
-  } else {
-    document.getElementById("response").innerText = "Try Again";
+function _0x3bb7(_0x4ec319, _0x434923) {
+  const _0x2c7b81 = _0x2c7b();
+  return (
+    (_0x3bb7 = function (_0x3bb7dc, _0x191765) {
+      _0x3bb7dc = _0x3bb7dc - 0xf4;
+      let _0x4fad27 = _0x2c7b81[_0x3bb7dc];
+      return _0x4fad27;
+    }),
+    _0x3bb7(_0x4ec319, _0x434923)
+  );
+}
+const _0x11f3b5 = _0x3bb7;
+(function (_0x326db1, _0x54ca79) {
+  const _0x5202a7 = _0x3bb7,
+    _0x3cbe1a = _0x326db1();
+  while (!![]) {
+    try {
+      const _0x4127cf =
+        parseInt(_0x5202a7(0x105)) / 0x1 +
+        parseInt(_0x5202a7(0x10f)) / 0x2 +
+        (parseInt(_0x5202a7(0x11a)) / 0x3) *
+          (-parseInt(_0x5202a7(0x113)) / 0x4) +
+        -parseInt(_0x5202a7(0x101)) / 0x5 +
+        -parseInt(_0x5202a7(0x117)) / 0x6 +
+        -parseInt(_0x5202a7(0x111)) / 0x7 +
+        parseInt(_0x5202a7(0xfb)) / 0x8;
+      if (_0x4127cf === _0x54ca79) break;
+      else _0x3cbe1a["push"](_0x3cbe1a["shift"]());
+    } catch (_0x38df07) {
+      _0x3cbe1a["push"](_0x3cbe1a["shift"]());
+    }
   }
-};
-
-const submitClue2 = (e) => {
-  e.preventDefault();
-  let a = document.getElementById("ans2").value;
-  console.log(a);
-  if (a.toLowerCase() == "engineer") {
-    document.getElementById("clue").innerHTML =
-      "Your answer was correct!!<br><br>Clue 3: Go to our Sanjay Singhania's Stack Overflow Profile and search fo the next clue.";
-    document.getElementById("clue2").setAttribute("hidden", "true");
-    document.getElementById("response").innerText = "";
-  } else document.getElementById("response").innerText = "Try Again";
-};
-
-document.getElementById("input1").addEventListener("click", submitClue1);
+})(_0x2c7b, 0x3feae);
+import "./style.css";
+(window[_0x11f3b5(0x109)] = function () {
+  return "All\x20progress\x20will\x20be\x20lost\x20if\x20you\x20leave!";
+}),
+  (window[_0x11f3b5(0x10a)] = function () {
+    const _0xed5f11 = _0x11f3b5;
+    document[_0xed5f11(0xf7)] = function (_0x22c2c8) {
+      const _0x577d2e = _0xed5f11;
+      return (
+        (_0x22c2c8[_0x577d2e(0x119)] || _0x22c2c8[_0x577d2e(0x114)]) != 0x74
+      );
+    };
+  });
+function _0x2c7b() {
+  const _0x1959c5 = [
+    "ans1",
+    "351242GpKGil",
+    "preventDefault",
+    "addEventListener",
+    "hidden",
+    "onbeforeunload",
+    "onload",
+    "clue",
+    "true",
+    "getElementById",
+    "Your\x20answer\x20was\x20correct!!<br><br>Clue\x203:\x20Go\x20to\x20our\x20Sanjay\x20Singhania\x27s\x20Stack\x20Overflow\x20Profile\x20and\x20search\x20fo\x20the\x20next\x20clue.",
+    "293236sKVZVf",
+    "stackoverflow",
+    "1170393LIUTEU",
+    "click",
+    "91092cnxIAA",
+    "keyCode",
+    "engineer",
+    "Try\x20Again",
+    "2400708LGbWsg",
+    "hint",
+    "which",
+    "24hhzrXj",
+    "stack\x20overflow",
+    "toLowerCase",
+    "input2",
+    "onkeydown",
+    "innerText",
+    "\x0a<div\x20id\x20=\x20\x22clue2\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<img\x20src\x20=\x20\x22../static/1.jpg\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<p>Sanjay\x20Singhania\x20recently\x20joined\x20Stack\x20Overflow.\x20Your\x20next\x20clue\x20is\x20hidden\x20in\x20his\x20Stack\x20Overflow\x20profile.\x20But\x20because\x20of\x20his\x20short\x20term\x20memory\x20loss\x20problem,\x20he\x20has\x20forgotten\x20his\x20Stack\x20Overflow\x20Display\x20Name.\x20All\x20he\x20remembers\x20is\x20this\x20meme\x20template\x20below\x20-</p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<img\x20src\x20=\x20\x22../static/2.jpg\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<p>Can\x20YOU\x20help\x20him\x20remember\x20his\x20username???</p>\x0a\x20\x20\x20\x20<form\x20id=\x22clue2-form\x22>\x0a\x20\x20\x20\x20\x20\x20<input\x20type=\x22text\x22\x20id=\x22ans2\x22\x20/>\x0a\x20\x20\x20\x20\x20\x20<input\x20type=\x22submit\x22\x20value=\x22Check\x22\x20id=\x22input2\x22\x20/>\x0a\x20\x20\x20\x20</form>\x0a\x20\x20\x0a</div>\x0a",
+    "value",
+    "7589424tFDrJl",
+    "response",
+    "log",
+    "ans2",
+    "clue2",
+    "setAttribute",
+    "2176155dorHDL",
+    "innerHTML",
+    "input1",
+  ];
+  _0x2c7b = function () {
+    return _0x1959c5;
+  };
+  return _0x2c7b();
+}
+const clue2 = _0x11f3b5(0xf9),
+  submitClue1 = (_0x27cef9) => {
+    const _0x10a6a8 = _0x11f3b5;
+    _0x27cef9[_0x10a6a8(0x106)]();
+    let _0x1a4529 = document[_0x10a6a8(0x10d)](_0x10a6a8(0x104))[
+      _0x10a6a8(0xfa)
+    ];
+    console[_0x10a6a8(0xfd)](_0x1a4529),
+      _0x1a4529["toLowerCase"]() == _0x10a6a8(0xf4) ||
+      _0x1a4529["toLowerCase"] == _0x10a6a8(0x110)
+        ? ((document[_0x10a6a8(0x10d)](_0x10a6a8(0xfc))[_0x10a6a8(0xf8)] = ""),
+          (document["getElementById"](_0x10a6a8(0x10b))[_0x10a6a8(0x102)] =
+            "Your\x20answer\x20was\x20correct!!<br><br>Here\x27s\x20your\x20next\x20clue."),
+          (document[_0x10a6a8(0x10d)](_0x10a6a8(0x118))[_0x10a6a8(0x102)] =
+            clue2),
+          document[_0x10a6a8(0x10d)](_0x10a6a8(0xf6))[_0x10a6a8(0x107)](
+            _0x10a6a8(0x112),
+            submitClue2
+          ))
+        : (document[_0x10a6a8(0x10d)](_0x10a6a8(0xfc))[_0x10a6a8(0xf8)] =
+            "Try\x20Again");
+  },
+  submitClue2 = (_0x2ab957) => {
+    const _0x26da63 = _0x11f3b5;
+    _0x2ab957[_0x26da63(0x106)]();
+    let _0x1a919d = document[_0x26da63(0x10d)](_0x26da63(0xfe))[
+      _0x26da63(0xfa)
+    ];
+    console[_0x26da63(0xfd)](_0x1a919d);
+    if (_0x1a919d[_0x26da63(0xf5)]() == _0x26da63(0x115))
+      (document["getElementById"](_0x26da63(0x10b))[_0x26da63(0x102)] =
+        _0x26da63(0x10e)),
+        document[_0x26da63(0x10d)](_0x26da63(0xff))[_0x26da63(0x100)](
+          _0x26da63(0x108),
+          _0x26da63(0x10c)
+        ),
+        (document[_0x26da63(0x10d)](_0x26da63(0xfc))["innerText"] = "");
+    else
+      document[_0x26da63(0x10d)](_0x26da63(0xfc))[_0x26da63(0xf8)] =
+        _0x26da63(0x116);
+  };
+document["getElementById"](_0x11f3b5(0x103))["addEventListener"](
+  _0x11f3b5(0x112),
+  submitClue1
+);
