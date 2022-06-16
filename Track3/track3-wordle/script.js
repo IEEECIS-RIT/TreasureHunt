a0fe41be4247fcf71a88c41fa13ad00fb7317d7a
Dhruv Dange <dhruvdange@gmail.com>
2022-06-16 20:15:48 +0530

    Added hint for wordle

--- Track3/track3-wordle/script.js
+++ Track3/track3-wordle/script.js
@@ -9,6 +9,32 @@ const msOffset = Date.now() - offsetFromDate;
 const dayOffset = msOffset / 1000 / 60 / 60 / 24;
 const targetWord = "eight";
 
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 startInteraction();
 
 function startInteraction() {
@@ -176,7 +202,7 @@ function shakeTiles(tiles) {
 
 function checkWinLose(guess, tiles) {
   if (guess === targetWord) {
-    document.querySelector(".heading").innerHTML = "https://bit.ly/3QhSfnl";
+    document.querySelector(".heading").innerHTML = "https://bit.ly/3b4yOhG";
     document.querySelector(".res").className += " fade";
     document.querySelector(
       ".res"
