885407d90172d544090bedf380bcee104144dd2c
Dhruv Dange <dhruvdange@gmail.com>
2022-06-16 13:29:14 +0530

    [UPDATED] Obfuscated

--- Final_Round/Hard/ABC.java
+++ Final_Round/Hard/ABC.java
@@ -6,7 +6,9 @@
 class ABC {
 
     public static void main(String[] args) {
-        // 555.444.22.777.2.777.999. : LIBRARY
+        // 555.444.22.777.2.777.999 : LIBRARY
+        // 4.444.8.44.88.22 : GITHUB
+        // int n = 8;
         int n = guessTheNumber();
         nextClue(n);
 
@@ -17,7 +19,8 @@ class ABC {
         // We take it seriously.
 
         // You have to write the correct value of n to proceed
-        int n = ?; // n ??????????????????????????????????????????
+        // TODO
+        int n = 8; 
         return n;
     }
 
